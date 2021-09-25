02b72b16273cdea4eb7ce6d5b0aad5b5d568f35e
George Gnativ <90177287+GeorrgeG@users.noreply.github.com>
2021-09-25 21:20:27 +0300

    Update task14.py

--- task14.py
+++ task14.py
@@ -7,6 +7,6 @@ def Common_key(dict1, dict2):
         for j in dict2.keys():
             if i == j:
                 print(i, 'is the common key')
-dict1={'Lesya':200,'Peter':20,'':250,'Shawn':100,'Sam':600}
-dict2={'Sam':600,'Oleg':450,'Lesya':200,'George':55,'Stas':20}
+dict1 = {'Lesya': 200, 'Peter': 20, 'Frodo': 250, 'Shawn': 100, 'Sam': 600}
+dict2 = {'Sam': 600, 'Oleg': 450, 'Lesya': 200, 'George': 55, 'Stas': 20}
 Common_key(dict1, dict2)
