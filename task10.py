b38a2ae5bae7165de5b9857c74ebb43157390f7f
George Gnativ <90177287+GeorrgeG@users.noreply.github.com>
2021-09-25 21:15:56 +0300

    Update task10.py

--- task10.py
+++ task10.py
@@ -2,7 +2,10 @@
 Question 10
 Write a program to find the key of the maximum value in a dictionary.
 '''
-dic = {'a': 25, 'b': 48, 'c': 90, 'd': 5, 'e':90}
-max_value = max(dic.values())  # maximum value
-max_keys = [k for k, v in dic.items() if v == max_value] # getting all keys containing the 'maximum'
+dic = {'a': 25, 'b': 48, 'c': 90, 'd': 5, 'e': 90}
+'maximum value'
+max_value = max(dic.values())  
+'getting all keys containing the maximum:'
+max_keys = [k for k, v in dic.items() if v == max_value]
 print(max_value, max_keys)
+
