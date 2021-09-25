8a961d4e8e753fbdbc042fec2bde13b52654b4c7
olenadurytska <90180454+olenadurytska@users.noreply.github.com>
2021-09-25 18:07:48 +0300

    Update task 8.py

--- task 8.py
+++ task 8.py
@@ -3,6 +3,7 @@ def econ(n):
         e = 300 + 100 * n
     else:
         m = n//6
-        e = 300 + 500*m + n*100
+        e = 300 + 500 * m + n * 100
     return e
-print(econ(10))
+n = int(input('Enter number of months: '))
+print(econ(n))
